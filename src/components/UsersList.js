34421eb6e294a30452587708406fa3679be343fe
Yaniv Ben Hamo <yaniv.bhemo@gmail.com>
2019-05-29 15:26:50 +0300

    db update

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet, Image } from 'react-native';
+import { View, Text, TextInput, Image, Button } from 'react-native';
 import axios from 'axios';
 import Card from './Card';
 import CardSection from './CardSection';
@@ -64,6 +64,20 @@ class UsersList extends Component{
         console.log(this.state);
         return (
             <View>
+                <Card>
+                    <CardSection>
+                        <View style={ styles.searchContainer }>
+                            <TextInput
+                                style={styles.searchInput}
+                                onChangeText={(text) => this.setState({text})}
+                                value={this.state.text}
+                            />
+                            <Button 
+                                title="Search"
+                            />
+                        </View>
+                    </CardSection>
+                </Card>
                 {this.renderUsers()}
             </View>
         );
@@ -83,6 +97,13 @@ class UsersList extends Component{
             </View> */}
 
 const styles = {
+    searchContainer: {
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+    },
+    searchInput: {
+        width: 260
+    },
     userItemStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around',
